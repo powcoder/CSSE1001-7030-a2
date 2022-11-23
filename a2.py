293da378cca0507089c1c37bbfadc1577e7eab54
powcoder <powcoder@163.com>
2022-11-23 12:37:51 +0800

    CSSE1001:7030 a2

---
+++ a2.py
@@ -0,0 +1,24 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+#!/usr/bin/env python3
+"""
+Assignment 2 - Sleeping Coders
+CSSE1001/7030
+Semester 2, 2019
+"""
+
+import random
+
+__author__ = "Your name & student number here"
+
+# Write your classes here
+
+
+def main():
+    print("Please run gui.py instead")
+
+
+if __name__ == "__main__":
+    main()
